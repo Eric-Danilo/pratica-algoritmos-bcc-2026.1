8729cff0a0bb0f4476d8996c5d87274bfbacefc6
Eric-Danilo <eric.danilo.cz@gmail.com>
2026-04-14 19:41:50 -0300

    Modifiquei o arquivo

--- 05.py
+++ 05.py
@@ -1,16 +1,11 @@
 num = int(input('Insira um numero: '))
-soma = 0
+soma = 0 
 
 
 while num != -1:
-    
-    if num < 21 or num > 70:
-        num = int(input('Insira um valor entre 21 e 70: '))
-    elif num >= 21 and num <= 70:
-        print('chegou aqui')
+
+    if num > 21 and num <= 70:
         num = int(input('Insira outro numero: '))
-        soma = soma + num
-    
-    
-        
-    soma = soma + num
+        soma = num + soma   
+    elif num < 21 or num > 70:
+        num = int(input('Insira um valor entre 21 e 70: '))
